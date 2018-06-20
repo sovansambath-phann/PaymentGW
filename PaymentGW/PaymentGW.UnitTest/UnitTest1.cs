3764a26a6685412558f96eecf0a25c8357d1c4f2
sovansambath.phann <sovansambath.phann@okoone.com>
2018-06-21 03:35:01 +0700

    add card controller ,implement console , add test case

---
+++ PaymentGW/PaymentGW.UnitTest/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentGW.Application.BounceContext.Card.DTO;
+using PaymentGW.Application.BounceContext.Card.Exceptions;
+using PaymentGW.Application.BounceContext.Card.Service;
+using PaymentGW.Application.BounceContext.Validation.DTO;
+using PaymentGW.Application.BounceContext.Validation.Service;
+
+
+namespace PaymentGW.UnitTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void Test_InValid_VisaCard_NotLeapYear() {
+            var cardNumber ="4111111111111111";
+            var expiryYear = 2019;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+            Assert.IsFalse(card.IsValid());
+            Assert.AreEqual(CardType.Visa,card.GetCardType());
+        }
+
+        [TestMethod]
+        public void Test_Valid_VisaCard_WithLeapYear() {
+            var cardNumber = "4111111111111111";
+            var expiryYear = 2020;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+            Assert.IsTrue(card.IsValid());
+            Assert.AreEqual(CardType.Visa, card.GetCardType());
+        }
+
+        [TestMethod]
+        public void Test_InValid_MasterCard_NotPrimeNumber() {
+            var cardNumber = "5111111111111111";
+            var expiryYear = 2020;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+            Assert.IsFalse(card.IsValid());
+            Assert.AreEqual(CardType.Master, card.GetCardType());
+        }
+
+        [TestMethod]
+        public void Test_Valid_MasterCard_PrimeNumber() {
+            var cardNumber = "5111111111111111";
+            var expiryYear = 2017;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+            Assert.IsTrue(card.IsValid());
+            Assert.AreEqual(CardType.Master, card.GetCardType());
+        }
+
+
+        [TestMethod]
+        public void Test_Valid_AmexCard_15Digit_StartWith3() {
+            var cardNumber = "311111111111111";
+            var expiryYear = 2019;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+            Assert.IsTrue(card.IsValid());
+            Assert.AreEqual(CardType.Amex, card.GetCardType());
+        }
+
+        [TestMethod]
+        public void Test_Valid_JCBCard_16Digit_StartWith3() {
+            var cardNumber = "3111111111111111";
+            var expiryYear = 2019;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+            Assert.IsTrue(card.IsValid());
+            Assert.AreEqual(CardType.JCB, card.GetCardType());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UnknownCardException))]
+        public void Test_NotFoundType_StartWith9() {
+            var cardNumber = "9111111111111111";
+            var expiryYear = 2019;
+            ACard card = CardFactory.GetCard(cardNumber, expiryYear);
+        }
+    }
+}
---
+++ PaymentGW/PaymentGW.WebAPI/Controllers/CardController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PaymentGW.Domain.BounceContext;
+using System.Data;
+using System.Data.Entity.Core.Objects;
+using log4net;
+using PaymentGW.WebAPI.Models;
+using PaymentGW.Application.BounceContext.Validation.DTO;
+using PaymentGW.Application.BounceContext.Validation.Service;
+using PaymentGW.Application.BounceContext.Card.DTO;
+using PaymentGW.Application.BounceContext.Card.Exceptions;
+using PaymentGW.Application.BounceContext.Card.Service;
+
+namespace PaymentGW.WebAPI.Controllers {
+    [RoutePrefix("api")]
+    public class CardController : ApiController {
+        ILog logger = log4net.LogManager.GetLogger("Error");
+
+        [HttpGet]
+        [Route("card/checkcard/{cardNumber}/{expiryYear}")]
+        public HttpResponseMessage Get(string cardNumber, string expiryYear) {
+            HttpResponseMessage response = null;
+            try {
+                IValidationService validationService = new ValidationService();
+                var validationResult = validationService.Validate(cardNumber,expiryYear);
+                if (!validationResult.IsValid) {
+                    response = new HttpResponseMessage() { 
+                        Content = new JsonContent(validationResult)
+                    };
+                }else {
+                    ICardService cardService = new CardService();
+                    int year = cardService.GetCardExpiryYear(expiryYear);
+
+                    ACard card = CardFactory.GetCard(cardNumber, year);
+                    var isValidCardNumber = card.IsValid();
+                    response = new HttpResponseMessage() {
+                        Content = new JsonContent(new PaymentGW.Application.BounceContext.Validation.DTO.ValidationResult() {
+                            CardNumber = cardNumber,
+                            IsValid = isValidCardNumber,
+                            Message = string.Empty,
+                            CardType = card.GetCardType()
+                        })
+                    };
+                }
+            }catch (UnknownCardException uEx) {
+                logger.Error(uEx);
+                response = new HttpResponseMessage() {
+                    Content = new JsonContent(new PaymentGW.Application.BounceContext.Validation.DTO.ValidationResult() {
+                        CardNumber = cardNumber,
+                        IsValid = false,
+                        Message = "Card is unknonw"
+                    })
+                };
+            }catch (Exception ex) {
+                logger.Error(ex);
+                
+                response = new HttpResponseMessage() {
+                    Content = new JsonContent(new PaymentGW.Application.BounceContext.Validation.DTO.ValidationResult() {
+                        CardNumber = cardNumber,
+                        IsValid = false,
+                        Message = "There are errors happens .Please contact the PaymentGW ."
+                    })
+                };
+
+            }
+
+            return response;
+        }
+    }
+}
